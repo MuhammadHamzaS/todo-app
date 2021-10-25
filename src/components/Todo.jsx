8a8d1bb807bef345c59f421e82c0c3aa7ef92fa8
Muhammad Hamza <hamza.tiwana92@gmail.com>
2021-10-25 14:34:46 -0700

    conditonal style rendering on updating a todo

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,8 +1,10 @@
 const Todo = ({ todo, onDelete, onUpdate }) => {
   return (
     <div className="todo">
-      <li className="todo-item">{todo.text}</li>
-      <button onClick={() => onUpdate(todo.id)} className="complete-btn ">
+      <li className={`todo-item ${todo.completed && "completed"}`}>
+        {todo.text}
+      </li>
+      <button onClick={() => onUpdate(todo.id)} className="complete-btn">
         <i className="fas fa-check"></i>
       </button>
       <button onClick={() => onDelete(todo.id)} className="trash-btn">
