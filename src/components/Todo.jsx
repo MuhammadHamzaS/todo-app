6ea715f8cb161f7987d936e3f8da10bf9d9a8d8e
Muhammad Hamza <hamza.tiwana92@gmail.com>
2021-10-25 14:07:55 -0700

    delete todo handler

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,12 +1,12 @@
 
-const Todo = ({todo}) => {
+const Todo = ({todo, onDelete}) => {
   return (
     <div className="todo">
       <li className="todo-item">{todo.text}</li>
-      <button className="complete-btn">
+      <button className="complete-btn ">
         <i className="fas fa-check"></i>
       </button>
-      <button className="trash-btn">
+      <button onClick={() => onDelete(todo.id)} className="trash-btn">
         <i className="fas fa-trash"></i>
       </button>
     </div>
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -3,10 +3,17 @@ import Todo from "./Todo";
 
 const TodoList = ({ todos, setTodos }) => {
     
+  // Delete a todo
+  const handleDelete = (id) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+
   return (
     <div className="todo-container">
       <ul className="todo-list">
-     {todos.map((todo) => <Todo key={todo.id} todo={todo} />)}
+        {todos.map((todo) => (
+          <Todo onDelete={handleDelete} key={todo.id} todo={todo} />
+        ))}
       </ul>
     </div>
   );
