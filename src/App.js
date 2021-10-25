b17cb860703b21d5c6e5c2adbf9feb3b19ef0579
Muhammad Hamza <hamza.tiwana92@gmail.com>
2021-10-25 12:56:46 -0700

    todo list and todo component layout

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
+// Import Components
 import Form from './components/Form';
+import TodoList from './components/TodoList';
 
 function App() {
   return (
     <div>
       <header>Todo List</header>
       <Form/>
+      <TodoList />
     </div>
   );
 }
